908751606fa4259980d2ae2792176486182f6a4b
Robert Mozayeni <dev@mozayeni.com>
2014-02-24 15:59:34 -0500

    Color and renaming of previous categories

---
+++ RSMToolkit/UIColor+RSMToolKit.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+RSMToolKit.h
+//  RSMToolkit
+//
+//  Created by Robert S Mozayeni on 2/24/14.
+//  Copyright (c) 2014 Robert Mozayeni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (RSMToolKit)
+
+@end
